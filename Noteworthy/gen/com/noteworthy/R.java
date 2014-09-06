f3181060533d350bb3aa92e3760baf93a287061c
michael7795 <michael7795@gmail.com>
2014-09-06 07:39:00 -0400

    save arezzo files

--- Noteworthy/gen/com/noteworthy/R.java
+++ Noteworthy/gen/com/noteworthy/R.java
@@ -1077,8 +1077,8 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
-        public static final int ic_plus=0x7f020058;
+        public static final int ic_add=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1092,18 +1092,20 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050040;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
-        public static final int add_button=0x7f05003f;
+        public static final int add_button=0x7f050047;
         public static final int always=0x7f05000b;
         public static final int androidActivities=0x7f05003c;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003e;
+        public static final int container=0x7f050045;
+        public static final int course_input=0x7f05003f;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
+        public static final int doneButton=0x7f050043;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
@@ -1120,9 +1122,11 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int professor_input=0x7f050042;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int school_input=0x7f05003e;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1140,7 +1144,10 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView=0x7f05003d;
+        public static final int textView=0x7f050044;
+        public static final int textViewCourse=0x7f050040;
+        public static final int textViewProfessor=0x7f050041;
+        public static final int textViewSchool=0x7f05003d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1197,14 +1204,16 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_classes=0x7f030018;
-        public static final int activity_main=0x7f030019;
-        public static final int fragment_main=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int activity_add_class=0x7f030018;
+        public static final int activity_classes=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int fragment_main=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
-        public static final int classes=0x7f0c0000;
-        public static final int main=0x7f0c0001;
+        public static final int add_class=0x7f0c0000;
+        public static final int classes=0x7f0c0001;
+        public static final int main=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1253,6 +1262,9 @@ containing a value of this type.
         public static final int hello_world=0x7f0a000f;
         public static final int title_activity_add_class=0x7f0a0011;
         public static final int title_activity_classes=0x7f0a0012;
+        public static final int title_course=0x7f0a0014;
+        public static final int title_professor=0x7f0a0015;
+        public static final int title_school=0x7f0a0013;
     }
     public static final class style {
         /** 
--- Noteworthy/src/com/noteworthy/Classes.java
+++ Noteworthy/src/com/noteworthy/Classes.java
@@ -1,9 +1,14 @@
 package com.noteworthy;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -15,7 +20,9 @@ public class Classes extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classes);
         setTitle("My Classes");
-
+        
+        android.app.ActionBar bar = getActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0099FF")));
 
     }
 
@@ -34,7 +41,7 @@ public class Classes extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.add_button) {
-
+        	createAddToast();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -42,15 +49,20 @@ public class Classes extends ActionBarActivity {
 
     public boolean createAddToast() {
 
-        CharSequence colors[] = new CharSequence[] {"red", "green", "blue", "black"};
+        CharSequence colors[] = new CharSequence[] {"Class", "Notes"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Class or Notes?");
+        builder.setTitle("What do you want to add?");
         builder.setItems(colors, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                // the user clicked on colors[which]
+            	if (which == 0){
+                	Intent myIntent = new Intent(Classes.this, AddClass.class);
+                	//myIntent.putExtra("key", value); //Optional parameters
+                	Classes.this.startActivity(myIntent);   
+            	}
             }
+
         });
         builder.show();
         return true;
