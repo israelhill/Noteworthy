c513e789648bfdc786ad637bc0df250bdf0e6a65
israelhill <idh@case.edu>
2014-09-06 09:11:37 -0400

    Login screen done

--- Noteworthy/src/com/noteworthy/MainActivity.java
+++ Noteworthy/src/com/noteworthy/MainActivity.java
@@ -2,14 +2,30 @@ package com.noteworthy;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.PopupMenu;
 import android.support.v4.app.Fragment;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.os.Build;
+
+import com.parse.GetCallback;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -17,12 +33,22 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        android.app.ActionBar bar = getActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0099ff")));
+        
+        Parse.initialize(this, "7NPKTk8T7KMyMGI97tRgHshB9LjwSvYXa1Z0RIzJ", "pqg8dny3PhjSrdXiKr7XPvc6rWwBy49I9h9TRozx");
+        
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
+        ParseObject fubu = new ParseObject("FuBu");
+        fubu.put("Israel", "Hill");
+        fubu.saveInBackground();
     }
 
 
@@ -45,7 +71,43 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    public void signMeUp(View v) {
+    	Intent intent = new Intent(this, SignUp.class);
+    	startActivity(intent);
+    }
+    
+    public void signIn(View view){
+    	
+    	EditText username = (EditText)findViewById(R.id.username);
+    	EditText password = (EditText)findViewById(R.id.password);
+    	
+    	ParseUser.logInInBackground(username.getText().toString(), password.getText().toString(), new LogInCallback() {
 
+			@Override
+			public void done(ParseUser uniqueUser, ParseException e) {
+				// TODO Auto-generated method stub
+				if (uniqueUser != null) {
+	    		      // Hooray! The user is logged in.
+						Intent intent = new Intent(MainActivity.this, Test.class);
+						startActivity(intent);
+	    		    } 
+					else {
+	    		      // Sign up failed. Look at the ParseException to see what happened.
+						AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+						builder.setMessage(R.string.invalid);
+						builder.setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
+					           public void onClick(DialogInterface dialog, int id) {
+					               // User clicked OK button
+					           }
+					       });
+						AlertDialog dialog = builder.create();
+						dialog.show();
+	    		    }
+	    		  }
+    		});
+    }
+    
     /**
      * A placeholder fragment containing a simple view.
      */
